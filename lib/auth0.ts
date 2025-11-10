713f561c26ce596a73a41e8987e68d31c80e471f
Marco Suarez <marcosuarezp95@gmail.com>
2025-11-10 09:00:05 -0400

    remove logs

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -2,8 +2,6 @@ import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
 export const auth0 = new Auth0Client({
   beforeSessionSaved: async (session, idToken) => {
-    console.log("BEFORE SESSION SAVED", session);
-    console.log(idToken);
     return session;
   },
 });
