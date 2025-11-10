de4757182c90f7aa96391d7ee8af92e9fabc5700
Marco Suarez <marcosuarezp95@gmail.com>
2025-11-10 16:49:47 -0400

    fix

--- app/api/handle-login/route.ts
+++ app/api/handle-login/route.ts
@@ -1,9 +1,6 @@
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
-export async function GET(
-  _: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
-) {
+export async function GET() {
   return redirect("/");
 }
