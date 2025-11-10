ef6a147fc3ba02e5e42a53a6fd8ddb7cfd7aa133
Marco Suarez <marcosuarezp95@gmail.com>
2025-11-10 17:09:03 -0400

    another try

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,7 +1,16 @@
+import { SdkError } from "@auth0/nextjs-auth0/errors";
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
+import { OnCallbackContext, SessionData } from "@auth0/nextjs-auth0/types";
+import { NextResponse } from "next/server";
 
 export const auth0 = new Auth0Client({
-  beforeSessionSaved: async (session, idToken) => {
-    return session;
+  onCallback: async (
+    error: SdkError | null,
+    ctx: OnCallbackContext,
+    session: SessionData | null
+  ) => {
+    return NextResponse.redirect(
+      new URL(ctx.returnTo ?? "/", process.env.APP_BASE_URL)
+    );
   },
 });
